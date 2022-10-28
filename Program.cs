2f946c8ca500fbd7d10051c54015165cee82f9b3
Daniel Júlio <daniel98julio@gmail.com>
2022-10-28 00:15:04 -0300

    Implementando o Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RhitmoPark.Application;
+
+namespace RhitmoPark
+{
+    class Program
+    {
+        static void Main()
+        {
+            InitialMenu.Menu();
+        }
+    }
+}
