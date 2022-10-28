66a7d3c5eb6727089d392f0838aef9af19167c1a
Daniel Júlio <daniel98julio@gmail.com>
2022-10-28 00:14:17 -0300

    Criando as Constantes da Camada de Application

---
+++ Application/Constants/ApplicationErrorMessagesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace RhitmoPark.Application.Constants
+{
+    public static class ApplicationErrorMessagesConstants
+    {
+        public const string EmptyConsoleInput = "Foi informado um valor vazio no console, favor informar um valor correto.";
+        public const string ConsoleInputNotInt = "Este campo só aceita valores do tipo inteiro, favor informar um valor correto.";
+        public const string WrongInput = "Você informou um valor diferente do esperado, favor informar um valor correto.";
+        public const string InvalidOption = "Escolha uma opção válida.";
+    }
+}
