08c30f491f74a1be36150b07e6a64f0a051bdcb0
Daniel Júlio <daniel98julio@gmail.com>
2022-10-28 00:14:43 -0300

    Criando a Camada De AntiCorruption

---
+++ AntiCorruption/ConsoleInputs.cs
@@ -0,0 +1,49 @@
+﻿using RhitmoPark.Application.Constants;
+
+namespace RhitmoPark.AntiCorruption
+{
+    public static class ConsoleInputs
+    {
+        public static string ReadLine()
+        {
+            try
+            {
+                var consoleInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(consoleInput))
+                {
+                    throw new Exception(ApplicationErrorMessagesConstants.EmptyConsoleInput);
+                }
+
+                return consoleInput;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ReadLine();
+            }
+        }
+
+        public static int IntReadLine()
+        {
+            try
+            {
+                var consoleInput = int.TryParse(Console.ReadLine(), out int parseResult);
+
+                if (!consoleInput)
+                {
+                    throw new Exception(ApplicationErrorMessagesConstants.ConsoleInputNotInt);
+                }
+
+                return parseResult;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return IntReadLine();
+            }
+        }
+    }
+}
